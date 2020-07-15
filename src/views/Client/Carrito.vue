eb7e54f08d7fd43bf75250e4e3c58a807582db5c
julio <juliosantos.26es@gmail.com>
2020-07-15 11:57:48 -0400

    agregar carrito incompleto

--- src/views/Client/Carrito.vue
+++ src/views/Client/Carrito.vue
@@ -1,25 +1,68 @@
 <template>
 	<div>
-		<template>
-			<div>
-				<b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
-				<b-sidebar id="sidebar-1" title="Sidebar" shadow>
-					<div class="px-3 py-2">
-						<p>
-							Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-							in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-						</p>
-						<span>Un cualquier cosa</span>
-						<b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
-					</div>
-				</b-sidebar>
-			</div>
-		</template>
+		<b-container>
+			<b-row>
+				<b-col cols="3" v-for="(producto, index) in productos" :key="index">
+					<template>
+						<v-card
+						class="mx-auto px-0"
+						max-width="200"
+						outlined
+						>
+						<v-img
+						height="200"
+						width="200"
+						src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+						></v-img>
+						<v-list-item three-line>
+							<v-list-item-content>
+								<div class="overline mb-4">{{producto.name}}</div>
+								<v-list-item-title class="headline mb-1">{{producto.precio}} Bs</v-list-item-title>
+							</v-list-item-content>
+							<v-list-item-avatar
+							tile
+							size="80"
+							color="grey"
+							></v-list-item-avatar>
+						</v-list-item>
+						<v-card-actions>
+							<b-button @click="agregarCarrito" variant="info">Agregar al carrito</b-button>
+						</v-card-actions>
+					</v-card>
+				</template>
+				</b-col>
+			</b-row>
+			<b-row>
+			carrito: {{ carrito }}
+			</b-row>
+		</b-container>
 	</div>
 </template>
 
 <script>
-export default{
-	name: 'Carrito'
-}
+	export default{
+		name: 'Carrito',
+		data(){
+			return{
+				carrito:[],
+				productos:[
+				{id:1, name:'manzana', precio:50},
+				{id:2, name:'cambur', precio:100},
+				{id:3, name:'pera', precio:75},
+				{id:4, name:'naranja', precio:90},
+				{id:4, name:'fresa', precio:30},
+				{id:4, name:'uvas', precio:25},
+				{id:4, name:'kiwi', precio:125},
+				{id:4, name:'Sandia', precio:200},
+				{id:4, name:'Carne', precio:700},
+				]
+			}
+		},
+		methods:{
+			agregarCarrito(index){
+				let i = this.productos[index]
+				this.carrito.push(i)
+			}
+		}
+	}
 </script>
